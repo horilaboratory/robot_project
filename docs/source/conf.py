fe6e5c9b74a53c81fcd99cbbe34e5beb67995631
GAI-313 <nakatogawagai@gmail.com>
2025-08-12 20:51:30 +0900

    テーマの変更

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme',
+    'pydata_sphinx_theme',
     'sphinx_tabs.tabs',
     'sphinx_copybutton',
     'sphinx_design',
@@ -33,7 +33,8 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_logo = "_static/logo.png"
 
